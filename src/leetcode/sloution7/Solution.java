b995c8f079ba6776fce847d98c2c6314d6f4b604
zhexueren <Liu_fengshan@126.com>
2019-01-02 23:55:13 +0800

    补充第7题的documnet文档

--- src/leetcode/sloution7/Solution.java
+++ src/leetcode/sloution7/Solution.java
@@ -2,6 +2,7 @@ package leetcode.sloution7;
 
 import java.util.ArrayList;
 
+
 /**
  * 
  * @Filename   :Solution.java
@@ -20,13 +21,55 @@ public class Solution
 	private int countNumBits(int num)
 	{
 		int count =0;
-		while (num!=0)
+		while (0 != num)
 		{
 			num &=num-1;
 			count++;
 		}
 		return count;
 	}
+	
+	private  int countNumBits_1(int num)
+	{
+		int count = 0;
+		while(0 != num)
+		{
+			if(0 != (num&0x1) )
+			{
+				count++;
+			}
+			num=num>>1;
+		}
+		
+		
+		return count;
+	}
+	
+	private  int countNumBits_2(int num)
+	{
+		int count = 0;
+		int mask  = 1;
+		int bitNum  = 0;
+		
+		if(0 == num)
+		{
+			return 0;
+		}
+		
+		bitNum = (int)(Math.log((double)num)/Math.log((double)2))+1;
+		
+		for(int i = 0; i<bitNum;i++)
+		{
+			mask = 1<<i;
+			if(0 != (num&mask))
+			{
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
     public int[] countBits(int num)
     {
     	if(num<0)
@@ -37,7 +80,7 @@ public class Solution
         
         for(int i = 0 ;i<=num;i++)
         {
-        	int count = countNumBits(i);
+        	int count = countNumBits_2(i);
         	data[i] = count;
         }
         
